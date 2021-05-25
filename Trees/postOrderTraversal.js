cfdb2f302946f83426f6a13aec0b6b92262e624a
Rahul Bojanapally <rahul.b246@gmail.com>
2021-05-26 01:00:30 +0530

    implement postorder traversal of BT iterative

---
+++ Trees/postOrderTraversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let stack = [root];
+    let result = [];
+    while(stack.length && stack[0] != null) {
+        let node = stack.pop();
+        result.push(node.val);
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+    return result.reverse();
+};
